2d4f96540dd618e11d5294c1a06e78483cd8fbbe
Vincenzo Mortillaro <vmortillaro@oakland.edu>
2022-11-08 14:36:53 -0500

    uploading gradecalc, js, and css files

---
+++ gradecalc.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <title>Class Grade Calculator</title>
+  </head>
+  <body>
+      <!-- Here this is setting up the colored pencils photo and the title 
+        for the page. You can begin editing outside of this but be careful -->
+      <div class="screenBackground">
+        <nav class="navbar">
+          <div class="logo">
+            <img src="img/Logo (1).jpg" width="80" height="80" />
+          </div>
+          <ul class="navItems">
+            <li class="tabNames">
+              <a href="index.html"> Home </a>
+            </li>
+            <li class="tabNames">
+              <a href="flashcards.html"> FlashCards </a>
+            </li>
+            <li class="tabNames">
+              <a href="toDoListPage.html"> Agenda </a>
+            </li>
+            <li class="tabNames">
+              <a href="classGrade.html"> Class Grade Calculator </a>
+            </li>
+            <li class="tabNames">
+              <a href="GPACalculator.html"> GPA Calculater </a>
+            </li>
+          </ul>
+          <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </nav>
+
+
+
+<!-- Insert you code here -->
+
+<?php
+
+//before displaying, check if numfields changed. Default to 1.
+if (is_numeric($_POST['numfields'])){
+	$numfields = intval($_POST['numfields']);
+} else {
+	$numfields = 1;
+}
+
+//Define the heredoc that holds your entire display
+//create the form
+$display = <<<display
+<form action='gradecalc2.php' method='POST'>
+display;
+
+//field prompting how many grade fields are being made
+$numfieldsfield = <<<numfieldsfield
+<label>How many assignments would you like to include?</label> <br />
+<input type='text' value=$numfields name='numfields'> <br />
+numfieldsfield;
+
+//add it to the display
+$display .= $numfieldsfield;
+
+
+//define arrays that will hold all other post value
+$storednames=array($numfields);
+$storedgrades=array($numfields);
+$storedtotals=array($numfields);
+
+//create a loop that outputs the correct number of fields
+
+//for each field desired...
+
+for ($index=0;$index<=($numfields-1); $index++){
+	//retrieve pre-existing data related to that field
+	$storednames[$index]=$_POST['assignmentname' . $index];
+	$storedgrades[$index]=intval($_POST['assignmentgrade' . $index]);
+	$storedtotals[$index]=intval($_POST['assignmenttotalpoints' . $index]);
+
+	//create one instance of an assignment info field
+	$fieldinstance = <<<fieldinstance
+<label style="flex-grow: 1">Assignment Name: </label>
+<input style="flex-grow: 1" type='text' value='$storednames[$index]' name='assignmentname$index'> <br />
+<label style="flex-grow: 1">Point total: </label>
+<input style="flex-grow: 1" type='text' value='$storedgrades[$index]' name='assignmentgrade$index'> <br />
+<label style="flex-grow: 1"> points out of </label>
+<input style="flex-grow: 1" type='text' value='$storedtotals[$index]' name='assignmenttotalpoints$index'> <br />
+<br /><br />
+fieldinstance;
+
+	//add it to the display
+	$display .= $fieldinstance;
+
+}
+
+//calculate your final grade based on the given data
+if ($_POST['process'] == 'Update'){
+	$pointscount = array_sum($storedgrades);
+	$maxcount = array_sum($storedtotals);
+	$finalgrade = -1;
+
+
+	if ($maxcount > 0){
+		$finalgrade = $pointscount/$maxcount;
+	} else {
+		$finalgrade = 0;
+	}
+	
+	//Convert to a whole number percentage
+	if (is_numeric($finalgrade)){
+		if ($finalgrade >= 0){
+			$percentage = ($finalgrade * 100);
+		} else {
+			$percentage = "ERROR";
+		}
+	} else {
+		$percentage = "ERROR";
+	}
+	$finalscoredisplay = <<<finalscoredisplay
+<label style="flex-grow: 1">Final Grade: </label> <br />
+<input style="flex-grow: 1" type='text' value='$percentage' name='finalgradetextbox'>&percnt;<br />
+finalscoredisplay;
+	
+	//add it to the display
+	$display .= $finalscoredisplay;
+}
+
+//add the end of the form and the submit button
+$calculatebutton = <<<calculatebutton
+<input type='submit' value='Update' name='process'>
+</form>
+calculatebutton;
+
+//add it to the display
+$display .= $calculatebutton;
+
+//show the display
+echo $display;
+?>
+
+</div>
+    <script src="project.js"></script>
+  </body>
+</html>
---
+++ project.js
@@ -0,0 +1,86 @@
+const navSlider = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".navItems");
+  const navLinks = document.querySelectorAll(".navItems li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+  //this is for all the information in the hamburger menu. When the three lines are clicked, this is listening for that action
+
+  //This is populating the code with the nav bar with a delay so all the tabs slide onto the screen for the user
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+  });
+};
+//Calling the function to run everything
+navSlider();
+
+
+//making the code for the password generator
+const lengthOfPass= document.getElementById('length');//here I am grabbing the length inputted by user
+const form_in_UI= document.getElementById('passwordGeneratorForm');//here I am grabbing the form being used by user
+const UpperCase1= document.getElementById('UpperCase');//here I am grabbing the checkbox that the user hopefully inputted
+const lowerCase1= document.getElementById('LowerCase');//here I am grabbing whether or not the user wants lower case letters
+const numberCase1= document.getElementById('NumberFilter');//here I am grabbing whether or not the user wants to include numbers 
+const SpecialCase1= document.getElementById('SpecialSymbols');//Here I am grabbing the section where the user decides whether or not they need special chars
+const passwordDisplay = document.getElementById('pass1')//here I am grabbing the section that will have the password
+const arrUpper=loopForAscii(65,90);//Here I am looping through the ASCII representations for Upper case letters and putting into an Array
+const arrLower=loopForAscii(97,122);//Here I am looping through the ASCII representations for lower case letters and putting into an Array
+const arrSpecial=loopForAscii(32,47).concat(loopForAscii(58,64)).concat(loopForAscii(91,96)).concat(loopForAscii(123,126));//Here I am looping through the ASCII representations for special characters and putting into an Array
+const arrNumbers=loopForAscii(48,57);//Here I am looping through the ASCII representations for numbers and putting into an Array
+//this function is for everytime the user presses the submit button it will continuously update the value
+function submitPassReq(){
+let length1=lengthOfPass.value
+const upper1=UpperCase1.checked//the .checked will just determine whether or not the user checked the box. The checked returns a boolean
+const lower1=lowerCase1.checked
+const special1=SpecialCase1.checked
+const num1=numberCase1.checked
+if(length1==""){//if the user doesnt anything in length, I will default it to length of 20 to ensure something will display on the screen
+  length1= 20
+}
+passwordDisplay.innerText = generatePass(length1,upper1, lower1,special1,num1)//generate password will be the function actually creating the password
+//I am taking the section that will display the password and updating it with the .innerText 
+
+}
+function generatePass(length1,upper1,lower1,special1,num1) {
+  let arryWCriteria=new Array()//this array will be populated depending on what the user inputs
+  //the if statements will begin to execute and the array will continuously be added to with the confirmation that the user needs specific criteria
+  if(lower1){
+    arryWCriteria = arryWCriteria.concat(arrLower)
+  }
+  if(upper1){
+    arryWCriteria = arryWCriteria.concat(arrUpper)
+  }
+ if(special1){
+  arryWCriteria = arryWCriteria.concat(arrSpecial)
+  }
+ if(num1){
+  arryWCriteria =  arryWCriteria.concat(arrNumbers)
+  }
+  //if the user does not enter anything into the form, this will default with all the criteria to be outputted by the system
+  else if(!upper1 & !lower1 & !special1 & !num1 ){
+    arryWCriteria = arryWCriteria.concat(arrUpper).concat(arrLower).concat(arrSpecial).concat(arrNumbers)
+  }
+  let passwordSymbols=new Array()//we will begin to populate the password with the specified symbols from the user
+  for(let i=0;i<length1;i++){
+    let randNum=arryWCriteria[Math.floor(Math.random()*arryWCriteria.length)]//I figured out this function from my sources listed
+    passwordSymbols.push(String.fromCharCode(randNum))//taking the random symbols and using the push function to put the random characters into th password
+  }
+  return passwordSymbols.join('')//the .join() function is so when the array is being printed, it usually prints with commas to separate each element. Adding the '' will just concat everything together
+}
+//this function is just put to make the process of populating each array easier. In my sources they threw everything in one array and that didnt work for the way I designed this UI
+//So I just went ahead and created this to organize everything. I got the idea for listed sources
+function loopForAscii(low,high){
+  const arr=[]
+  for(let i=low;i<=high;i++){
+    arr.push(i);
+  }
+  return arr;
+}
+const copyBtnVar = document.getElementById('copyBtn')
+//passwordDisplay is the variable here that the clipboard will want to copy
+function CopyThePassword(){//when user wants to copy their password
+navigator.clipboard.writeText(passwordDisplay.innerText);//this will pull the passwords value and add it to the users clipboard
+alert('Text copied');//this will let the user know that they have successfully copied their new password
+}
